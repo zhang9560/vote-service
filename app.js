133732dc4ced93b6a4b92e830339299fb5672e64
Yanghai Zhang <zhang9560@outlook.com>
2017-01-24 15:11:53 +0800

    可以创建投票

--- app.js
+++ app.js
@@ -1,34 +1,68 @@
 var express = require('express');
+var uuid = require('uuid');
 var bodyParser = require('body-parser');
 var app = express();
 var jsonParser = bodyParser.json();
 
-var voteResult = [];
+var voteResults = {};
+var voteItems = {};
 
 app.get('/', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.send('Hello World!');
+    res.send();
 }).get('/result', function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    let voteId = req.query['id'];
+    let voteResult = voteResults[voteId];
     let resBody = new Object();
-    for (let index in voteResult) {
-        let item = voteResult[index].item;
-        if (resBody.hasOwnProperty(item)) {
-            resBody[item]++;
-        } else {
-            resBody[item] = 1;
+
+    if (voteResult !== undefined) {
+        for (let index in voteResult) {
+            let item = voteResult[index].item;
+            if (resBody.hasOwnProperty(item)) {
+                resBody[item]++;
+            } else {
+                resBody[item] = 1;
+            }
         }
     }
+    console.log(resBody);
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.send(resBody);
+}).get('/get_vote', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    let voteId = req.query['id'];
+    if (voteId === undefined) {
+        res.send({result: 1, errMsg: 'no query named id'});
+    } else {
+        res.send({result: 0, id: voteId, data: voteItems[voteId]});
+    }
 });
 
 app.post('/vote', jsonParser, function (req, res) {
     console.log(req.body);
+    let voteId = req.body.id;
+    delete req.body.id;
+
+    let result = voteResults[voteId];
+    if (result === undefined) {
+        result = [req.body];
+        voteResults[voteId] = result;
+    } else {
+        result.push(req.body);
+    }
+    console.log(result);
+    
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'content-type');
-    res.send('');
-
-    voteResult.push(req.body);
+    res.send({result: 0});
+}).post('/create_vote', jsonParser, function (req, res) {
+    console.log(req.body);
+    let voteId = uuid.v4();
+    voteItems[voteId] = req.body;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type');
+    res.send({result: 0, id: voteId});
 });
 
 app.options('*', function(req, res) {
