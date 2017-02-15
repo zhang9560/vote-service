5f188ec73da1b3948c975e88c0b0048ca67f9e2c
Yanghai Zhang <zhang9560@outlook.com>
2017-02-15 15:07:42 +0800

    change port

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.options('*', function(req, res) {
     res.send();
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(5000, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
